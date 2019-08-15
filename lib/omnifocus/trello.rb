be14bd7c1448c8697bdd700a4c21e448443d4c87
Baul <roidinev@gmail.com>
2019-08-15 11:13:16 +0800

    add some features
    1. change to use bug_db hash from array
    2. use label red as flagged in of
    3. use label green as nested tag "When : Today" in of
       ref: https://inside.omnifocus.com/blog/the-forecast-tag.
    4. use due as due_date in of
    5. update of task once trello care have been updated according
       card field dateLastActivity.
    Now  todos:
        defer data maybe use the powerup feature.
        two-way sync.

--- lib/omnifocus/trello.rb
+++ lib/omnifocus/trello.rb
@@ -1,6 +1,7 @@
 require "open-uri"
 require "json"
 require "yaml"
+require "digest/md5"
 
 module OmniFocus::Trello
   PREFIX  = "TR"
@@ -24,6 +25,7 @@ module OmniFocus::Trello
   end
 
   def populate_trello_tasks
+    print "populating tasks"	  
     config     = load_or_create_trello_config
     token      = config[:token]
     done_lists = config[:done_lists] || config[:done_boards]
@@ -40,30 +42,103 @@ module OmniFocus::Trello
     JSON.parse(open(url).read)
   end
 
+  # get nested tag "When : Today", which need create in of. 
+  def gettag()
+          omnifocus.tags.get.each do |tagg|
+	            tagg.tags.get.each do |t|
+			    if t.name.get == "Today" 
+				    return t
+			    end
+		    end 
+
+          end
+
+  end 
+
   def process_trello_card(boards, done_lists, card)
     number       = card["idShort"]
+
+    datelast  = card["dateLastActivity"]
+    hex = Digest::MD5.hexdigest(datelast)
+
     description  = if card["desc"].length > 0
       card["shortUrl"] + "\n\n" + card["desc"]
     else
       card["shortUrl"]
     end
+
+    description = hex + "\n" + description 
+			   
+    today  =  nil
+    flag = false 
+
+    # label red meam flagged in of, label green mean Today tag in of.
+    if !card["labels"].empty?
+      card["labels"].each { |l|  if l["color"]== "green"
+  			                today = "Today"
+  			         end
+                                 if l["color"] == "red"
+  				        flag = true
+			         end
+      }
+    end
+
+                   
+    due          = card["due"]
     board        = boards.find {|candidate| candidate["id"] == card["idBoard"] }
     project_name = board["name"]
     ticket_id    = "#{PREFIX}-#{project_name}##{number}"
     title        = "#{ticket_id}: #{card["name"]}"
     list         = board["lists"].find {|candidate| candidate["id"] == card["idList"] }
+    
+
+
+    
 
     # If card is in a "done" list, mark it as completed.
     if done_lists.include?(list["name"])
       return
     end
 
+    puts ticket_id      
     if existing[ticket_id]
-      bug_db[existing[ticket_id]][ticket_id] = true
-      return
+      # of note format:
+      #	 datelastactivity
+      #	 descriptinn or url
+      
+      # get note from of and compare
+      hex = hex + "\n"
+      if update[ticket_id] == hex 
+          bug_db[existing[ticket_id]][ticket_id] = true
+          return
+      else 
+	      #if the card have been updated, not mark it complete ,we remove the card ,and then readd the card 
+              project = nerd_projects.projects[project_name]
+	      project.tasks[its.name.contains(ticket_id)].get.each do |task|
+                puts "deletingg #{ticket_id}, will readd latter into of"
+                task.delete
+              end
+      end
     end
 
-    bug_db[project_name][ticket_id] = [title, description]
+   if due 
+      t = Time.parse(due)
+   end
+   if today != nil 
+       if t 
+         bug_db[project_name][ticket_id] = { :name => title, :note => description, :due_date => t, :flagged => flag, :primary_tag => gettag()}
+       else
+         bug_db[project_name][ticket_id] = { :name => title, :note => description, :flagged => flag, :primary_tag => gettag()}
+       end
+   else 
+       if t 
+          bug_db[project_name][ticket_id] = { :name => title, :note => description, :due_date => t, :flagged => flag}
+       else
+          bug_db[project_name][ticket_id] = { :name => title, :note => description,  :flagged => flag}
+       end
+
+   end
+
   end
 
   def fetch_trello_boards(token)
